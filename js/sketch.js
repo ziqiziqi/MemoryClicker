76bec16081d0ea3c57e195fc84d9ee21950f30ce
Pan Chen <prilochen@126.com>
2021-12-06 13:04:54 +0800

    add sqlite

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,7 @@
 let olddate,oldmemory,oldpoints,newpoints;
 let notebg,font,paperplane;
 let sel,s,button,update,like,dislike,liketext,disliketext;
+var isFresh = true;
 
 
 function preload(){
@@ -37,7 +38,8 @@ function setup() {
   sel.changed(mySelectEvent);
   btn = document.getElementById("btn");
   btn.addEventListener('click',function(event){
-    record();
+    //record();
+    recordTest();
     //effect();
   });
 }
@@ -47,9 +49,16 @@ function mySelectEvent() {
   console.log("select points!"+oldpoints);
 }
 
+//only for test by cp
+function recordTest(){
+  console.log("submit!")
+  sqlTest();
+  fetchNotes();
+}
 
 function record() {
   console.log("submit!")
+  sqlTest();
   fetchNotes();
   sel.remove();
   clear();
@@ -112,6 +121,77 @@ function likeevent(){
 
 function dislikeevent(){
   newpoints = oldpoints-1;
+}
+
+//sqlite part
+function errorHandler(transaction, error)
+{
+    // error.message is a human-readable string.
+    // error.code is a numeric error code
+    //alert('Oops.  Error was '+error.message+' (Code '+error.code+')');
+    console.log('Oops.  Error was '+error.message+' (Code '+error.code+')');
+ 
+    // Handle errors here
+    var we_think_this_error_is_fatal = true;
+    if (we_think_this_error_is_fatal) return true;
+    return false;
+}
+ 
+function dataHandler(transaction, results)
+{
+    // Handle the results
+    var string = "Green shirt list contains the following people:\n\n";
+    for (var i=0; i<results.rows.length; i++) {
+        // Each row is a standard JavaScript array indexed by
+        // column names.
+        var row = results.rows.item(i);
+        string = string + row['name'] + " (ID "+row['id']+")\n";
+    }
+    console.log(string);
+    // alert(string);
+}
+
+
+function nullDataHandler(transaction, results) { 
+  console.log("sql error");
+}
+ 
+function createTables(db)
+{
+    db.transaction(
+        function (transaction) {
+ 
+            /* The first query causes the transaction to (intentionally) fail if the table exists. */
+            transaction.executeSql('CREATE TABLE idea(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL DEFAULT "Be happy", score int NOT NULL DEFAULT 5, date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);', [], nullDataHandler, errorHandler);
+            /* These insertions will be skipped if the table already exists. */
+            transaction.executeSql('insert into idea (name, score) VALUES ("work", 1);', [], nullDataHandler, errorHandler);
+            transaction.executeSql('insert into idea (name, score) VALUES ("sleep", 1);', [], nullDataHandler, errorHandler);
+            transaction.executeSql('insert into idea (name, score) VALUES ("eat", 2);', [], nullDataHandler, errorHandler);
+            transaction.executeSql('insert into idea (name, score) VALUES ("skate", 3);', [], nullDataHandler, errorHandler);
+        }
+    );
+}
+
+function sqlTest(){
+  console.log("begin sql test");
+  var shortName = 'mydatabase';
+  var version = '1.0';
+  var displayName = 'My Important Database';
+  var maxSize = 65536; // in bytes
+  var db = openDatabase(shortName, version, displayName, maxSize);
+  if(isFresh){
+    console.log("create database");
+    createTables(db);
+    isFresh = false;
+  }
+  
+  db.transaction(
+    function (transaction) {
+        transaction.executeSql("SELECT * from idea where score=1;",
+            [], // array of values for the ? placeholders
+            dataHandler, errorHandler);
+    }
+);
 }
 
 // // chrome.storage.sync.set({key: value}, function() {
